0bdccf5abb59b3ac82e506ec56931be57ae7a3f1
Harsha <pharshavardhan2@outlook.com>
2024-02-24 18:33:25 +0530

    added static middleware

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 const app = express()
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 morgan.token('req-body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
